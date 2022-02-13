26c3272210548fd9403d840e924624c6f79339eb
Smatodea_Pha <365639685@qq.com>
2022-02-13 15:57:52 +0800

    react个人网站练习

---
+++ components/Skills.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Skills() {
+    return(
+        <h1>hello world</h1>
+     )
+}
---
+++ components/Testimonials.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Testimonials() {
+    return(
+        <h1>hello world</h1>
+     )
+}
